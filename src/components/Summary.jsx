89c1921c204ccce9fda352822af1c92913ea4a2c
Melaniemsc <melsan1706@gmail.com>
2024-07-15 09:22:14 +0100

    added firlter to the sumary of country visas

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,115 +3,212 @@ import { Link } from "react-router-dom"
 import { getPayload } from "../lib/auth"
 import axios from "axios"
 import { Dropdown } from 'primereact/dropdown';
-import {baseUrl} from '../config'
+import { baseUrl } from '../config'
 
 const Summary = () => {
-
-  const [user, setUser] = useState({ first_name: '', nationality: [] })
-  const [countries, setCountries] = useState([])
-  const [selectedCountry, setSelectedCountry] = useState('')
-  const [countryVisaRelations, setCountryVisaRelations] = useState([])
-
+  const [user, setUser] = useState({ first_name: '', nationality: [] });
+  const [countries, setCountries] = useState([]);
+  const [selectedAddCountry, setselectedAddCountry] = useState('');
+  const [visaRequiremenstCountryFrom, setVisaRequiremenstCountryFrom] = useState([]);
+  const [countryVisaRelations, setCountryVisaRelations] = useState([]);
+  const [order, setOrder] = useState(true);
+  const [typesOfVisa, setTypesOfVisa] = useState([]);
+  const [selectedVisaType, setSelectedVisaType] = useState('');
+  const [countryOrder, setCountryOrder] = useState('');
 
   async function fetchUser() {
-    const token = localStorage.getItem('token')
-    const resp = await axios.get(`${baseUrl}/api/auth/summary/`, { headers: { Authorization: `Bearer ${token}` } })
-    setUser(resp.data)
-    /* console.log(resp.data); */
+    const token = localStorage.getItem('token');
+    const resp = await axios.get(`${baseUrl}/api/auth/summary/`, { headers: { Authorization: `Bearer ${token}` } });
+    setUser(resp.data);
+    setVisaRequiremenstCountryFrom(resp.data.nationality);
   }
 
   async function fetchCountries() {
-    const resp = await axios.get(`${baseUrl}/api/countries/`)
-/*     console.log(resp.data);
- */    setCountries(resp.data)
-    return countries
+    const resp = await axios.get(`${baseUrl}/api/countries/`);
+    setCountries(resp.data);
   }
 
   async function handleAddButton() {
-    const token = localStorage.getItem('token')
-    console.log(selectedCountry);
-    await axios.put(`${baseUrl}/api/auth/summary/`, { nationality: selectedCountry.name }, {
+    const token = localStorage.getItem('token');
+    await axios.put(`${baseUrl}/api/auth/summary/`, { nationality: selectedAddCountry.name }, {
       headers: { Authorization: `Bearer ${token}` }
-    })
-    setSelectedCountry("Select a Country")
+    });
+    setselectedAddCountry(null);
+    fetchUser();
   }
 
   async function handleDelete(nationality) {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token');
     await axios.delete(`${baseUrl}/api/auth/summary/`, {
       headers: { Authorization: `Bearer ${token}` },
-      data: { nationality: nationality.nationality }
-    })
-    fetchUser()
+      data: { country: nationality.name }
+    });
+    fetchUser();
   }
 
   async function handleVisa() {
-    const token = localStorage.getItem('token')
-    user.nationality.map(async (nationality) => {
-      const resp = await axios.get(`${baseUrl}api/visa/?country_from=${nationality.name}`, {
-        Authorization: `Bearer ${token}`
-      })
-      setCountryVisaRelations(resp.data)
-      console.log(resp.data);
-    });
+    const token = localStorage.getItem('token');
+    const queryParameter = user.nationality.map(nationality => nationality.name).join(',');
+    if (queryParameter) {
+      try {
+        const resp = await axios.get(`${baseUrl}/api/visa/?country_from=${queryParameter}`, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+        setCountryVisaRelations(resp.data);
 
-};
-
-
+        const visaTypes = resp.data.reduce((types, item) => {
+          item.visa_type.forEach(visa => {
+            if (!types.includes(visa.type)) {
+              types.push(visa.type);
+            }
+          });
+          return types;
+        }, []);
+        setTypesOfVisa(visaTypes);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 
   useEffect(() => {
-    fetchUser()
-    fetchCountries()
-    handleVisa()
-  }, [selectedCountry])
+    fetchUser();
+    fetchCountries();
+  }, []);
 
+  useEffect(() => {
+    if (user.nationality.length > 0) {
+      handleVisa();
+    }
+  }, [user.nationality]);
 
-  return <div className="hero is-fullheight-with-navbar">
-    <div className="hero-body">
-      <div className="container columns">
-        <div className="column">
-          <h1 className="subtitle mb-5">
-            Hello {user.first_name}!
-          </h1>
-          <div>
-            <div className="columns">
-              <div className="column">
-                <h2>These are your nationalities</h2>
-              </div>
-              <div className="column">
-                <Dropdown
-                  value={selectedCountry}
-                  onChange={(e) => setSelectedCountry(e.value)}
-                  options={countries}
-                  optionLabel="name"
-                  placeholder="Select a Country"
-                  filter
-                  className="w-full md:w-14rem" />
-                <button onClick={handleAddButton}>➕</button>
-              </div>
-            </div>
+  function handleOrder() {
+    setOrder(!order);
+  }
+
+  function handleReset() {
+    setCountryOrder('');
+    setSelectedVisaType('');
+    setOrder(true);
+  }
+
+  const handleCountryOrder = (country) => {
+    setCountryOrder(country.name);
+  };
+
+  const filteredCountryVisaRelations = countryOrder
+    ? countryVisaRelations.filter(relation => relation.country_from.name === countryOrder)
+    : countryVisaRelations;
+
+  function orderByCountry() {
+    return (
+      <div className="buttons">
+        {visaRequiremenstCountryFrom.map(country => (
+          <div key={country.name} className="button" onClick={() => handleCountryOrder(country)}>
+            {country.name}
           </div>
-          {user.nationality.map((nationality, index) => {
-            return <div key={index}>
-              {nationality.nationality}
-              <button className="block" onClick={() => handleDelete(nationality)}>❌</button>
+        ))}
+        <div>
+          {filteredCountryVisaRelations.map((relation, index) => (
+            <div className="box" key={index}>
+              <h3>{relation.country_from.name}</h3>
+              <h3>{relation.country_to.name}</h3>
+              {relation.visa_type.map((visa, index) => (
+                <div key={index}>
+                  <h3>Visa type: {visa.type}</h3>
+                </div>
+              ))}
+              {relation.allowed_stay !== "nan" && <h3>Allowed Stay: {relation.allowed_stay}</h3>}
+              {relation.notes !== "nan" && <h3>Extra notes: {relation.notes}</h3>}
             </div>
-          })}
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  const filteredByVisaType = selectedVisaType
+    ? filteredCountryVisaRelations.filter(relation => 
+        relation.visa_type.some(visa => visa.type === selectedVisaType))
+    : filteredCountryVisaRelations;
+
+    function orderByVisaType() {
+      return (
+        <div>
+          <Dropdown
+            value={selectedVisaType}
+            onChange={(e) => setSelectedVisaType(e.value)}
+            options={typesOfVisa.map(type => ({ label: type, value: type }))}
+            placeholder="Select type of visa"
+            filter
+            className="w-full md:w-14rem"
+          />
           <div>
+            {filteredByVisaType.map((relation, index) => (
+              <div className="box" key={index}>
+                <h3>{relation.country_from.name}</h3>
+                <h3>{relation.country_to.name}</h3>
+                {relation.visa_type.map((visa, index) => (
+                  <div key={index}>
+                    <h3>Visa type: {visa.type}</h3>
+                  </div>
+                ))}
+                {relation.allowed_stay !== "nan" && <h3>Allowed Stay: {relation.allowed_stay}</h3>}
+                {relation.notes !== "nan" && <h3>Extra notes: {relation.notes}</h3>}
+              </div>
+            ))}
           </div>
         </div>
-        <div className="column">
-          <h2 className="title is-5">Visa Requirements</h2>
-          {countryVisaRelations.map((relation, index) =>{
-            return <div className="box" key={index}>
-              <p>{relation.country_to.name} </p>
+      );
+    }
 
+  return (
+    <div className="hero is-fullheight-with-navbar">
+      <div className="hero-body">
+        <div className="container columns">
+          <div className="column">
+            <h1 className="subtitle mb-5">Hello {user.first_name}!</h1>
+            <div>
+              <div className="columns">
+                <div className="column">
+                  <h2>These are your nationalities</h2>
+                </div>
+                <div className="column">
+                  <Dropdown
+                    value={selectedAddCountry}
+                    onChange={(e) => setselectedAddCountry(e.value)}
+                    options={countries}
+                    optionLabel="name"
+                    placeholder="Select a Country"
+                    filter
+                    className="w-full md:w-14rem"
+                  />
+                  <button onClick={handleAddButton}>➕</button>
+                </div>
+              </div>
             </div>
-          })}
+            {user.nationality.map((nationality, index) => (
+              <div key={index}>
+                {nationality.name}
+                <button className="block" onClick={() => handleDelete(nationality)}>❌</button>
+              </div>
+            ))}
+          </div>
+          <div className="column">
+            <h2 className="title is-5">Visa Requirements</h2>
+            <div className="buttons">
+              <button className="button" onClick={handleOrder}>{order ? "Order by Country" : "Order by VisaType"}</button>
+              <button className="button" onClick={handleReset}>Reset</button>
+            </div>
+            <div>
+              {order && orderByCountry()}
+              {!order && orderByVisaType()}
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default Summary
+export default Summary;
